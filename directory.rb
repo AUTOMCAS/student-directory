bce620765adaec5a9960bad4b31ac5976c7e959b
Chris <caston88@gmail.com>
2022-09-05 14:41:25 +0100

    Update directory.rb

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-#Put students into an array
 students = [
  "Dr. Hannibal Lecter",
  "Darth Vader",
@@ -12,12 +11,22 @@ students = [
  "Joffrey Baratheon",
  "Norman Bates" 
 ]
-# print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student| 
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-# Finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name| 
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
